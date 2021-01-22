48064451dd1a7ced86c195b1d122c67535488a90
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-22 18:07:20 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,8 +3,6 @@
 describe "./calculator.rb" do
 
   it "contains a local variable called first_number that is assigned to a number" do
-    def first_number
-    
     first_number = get_variable_from_file('./calculator.rb', "first_number")
 
     expect(first_number).to be_an(Integer).or be_a(Float)
